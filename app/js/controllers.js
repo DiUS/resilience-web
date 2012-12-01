6bf95cfbdfaa084437601c02d490749a09c8a16a
Alex <oburakevych@gmail.com>
2012-12-01 19:46:20 +1100

    Added map view

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -7,3 +7,44 @@ function IssueListController($scope, $rootScope, IssueService) {
 function IssueController() {
 	
 }
+
+function MapController($scope, MapService, IssueService) {
+	$scope.issueList = IssueService.get();
+	
+	
+	
+	$scope.markLocation = function() {
+			var lat = document.getElementById("txtLat");
+			var lng = document.getElementById("txtLng");
+			var issue = "text";
+	        var location = new google.maps.LatLng(lat.value, lng.value);
+
+			var mapOptions = {
+				zoom: 6,
+				center: location,
+				mapTypeId: google.maps.MapTypeId.ROADMAP
+			};
+
+	        var map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
+
+	        var marker = new google.maps.Marker({
+	            position: location,
+	            map: map,
+	            title: issue		
+			});
+	  }
+	
+	$scope.getBoundaries = function() {
+		var boundaries = {
+		};
+		
+		if ($scope.issueList) {
+			angular.forEach($scope.issueList.results, function(issue, index) {
+				if (issue.location) {
+					
+				}
+			});
+		}
+	}
+
+}
